4397cc10899d77b13e3318c3fd5562ddb69b6306
YangMingyu <1830574935@qq.com>
2024-04-28 17:54:01 +0800

    Delete

--- sayhello/sayhello/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteNote(noteId) {
-  fetch("/note/delete-note", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "/note";
-  });
-}
